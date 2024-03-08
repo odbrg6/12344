951edda2d137f213ed68c5f12419aabab6e9fc7b
odbrg6 <99286663+odbrg6@users.noreply.github.com>
2024-03-08 12:36:22 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 class Config:
-    API_ID = os.environ.get("API_ID")
-    API_HASH = os.environ.get("API_HASH")
-    TOKEN = os.environ.get("TOKEN")
-    START_PIC = os.environ.get("START_PIC")
-    CHAT = os.environ.get("CHAT")
+    API_ID = os.environ.get("22877012")
+    API_HASH = os.environ.get("6d59bf3287c22f491e291a8a366c997c")
+    TOKEN = os.environ.get("6971545809:AAHPark1t-daGM_pXqqzohvxZ3r5cSQUyOs")
+    START_PIC = os.environ.get("https://graph.org/file/9ffcff556e24e0c37e444.jpg")
+    CHAT = os.environ.get("-1002134604531")
 
 
 
